97e3dfe847159db1c51ce6d408245c9230b7e3df
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-09-02 10:47:02 +0300

    declare the constant and variables

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // 
+// variables and constants
+const cartContainer = document.querySelector('.cart-container');
+const productList = document.querySelector('.product-list');
+const cartList = document.querySelector('.cart-list');
+const cartTotalValue = document.getElementById('cart-total-value');
+const cartCountInfo = document.getElementById('cart-count-info');
+let cartItemID = 1;
