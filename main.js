07804902bf16e036860930cb4cecae864aad3634
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-09-02 12:26:49 +0300

    add images

--- main.js
+++ main.js
@@ -60,3 +60,4 @@ function loadJSON(){
         //URL scheme must be "http" or "https" for CORS request. You need to be serving your index.html locally or have your site hosted on a live server somewhere for the Fetch API to work properly.
     })
 }
+
